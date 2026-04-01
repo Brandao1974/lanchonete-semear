cfadcacbc4a6af8ee88a96901363ca45e8e08356
Ildebrando <ildebrando.s@gmail.com>
2026-04-01 11:06:56 -0300

    Adicionando PWA Service Worker

---
+++ service-worker.js
@@ -0,0 +1,25 @@
+const cacheName = "semear-v1";
+
+const assets = [
+"./",
+"./index.html",
+"./style.css",
+"./script.js",
+"./manifest.json"
+];
+
+self.addEventListener("install", e => {
+e.waitUntil(
+caches.open(cacheName)
+.then(cache => cache.addAll(assets))
+);
+});
+
+self.addEventListener("fetch", e => {
+e.respondWith(
+caches.match(e.request)
+.then(response => {
+return response || fetch(e.request);
+})
+);
+});
