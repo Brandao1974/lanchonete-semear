2c65d30ded9e5b9257149c98da0676a554199c85
Ildebrando <ildebrando.s@gmail.com>
2026-04-01 11:39:09 -0300

    Adicionando favicon e PWA completo

--- service-worker.js
+++ service-worker.js
@@ -5,7 +5,10 @@ const assets = [
 "./index.html",
 "./style.css",
 "./script.js",
-"./manifest.json"
+"./manifest.json",
+"./icons/favicon-96x96.png",
+"./icons/icon-192.png",
+"./icons/icon-512.png"
 ];
 
 self.addEventListener("install", e => {
